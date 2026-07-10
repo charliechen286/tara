15b51e15992db4b0a68d99378700f9bb43d89bd9
Charlie Chen <151472175+charliechen286@users.noreply.github.com>
2026-07-10 04:16:10 -0700

    Add files via upload

---
+++ data/gallery.js
@@ -0,0 +1,63 @@
+const memories = [
+
+{
+    image:"../assets/photos/photo1.jpg"
+},
+
+{
+    image:"../assets/photos/photo2.jpg"
+},
+
+{
+    image:"../assets/photos/photo3.jpg"
+},
+
+{
+    image:"../assets/photos/photo4.jpg"
+},
+
+{
+    image:"../assets/photos/photo5.jpg"
+},
+
+{
+    image:"../assets/photos/photo6.jpg"
+},
+
+{
+    image:"../assets/photos/photo7.jpg"
+},
+
+{
+    image:"../assets/photos/photo8.jpg"
+},
+
+{
+    image:"../assets/photos/photo9.jpg"
+},
+
+{
+    image:"../assets/photos/photo10.jpg"
+},
+
+{
+    image:"../assets/photos/photo11.jpg"
+},
+
+{
+    image:"../assets/photos/photo12.jpg"
+},
+
+{
+    image:"../assets/photos/photo13.jpg"
+},
+
+{
+    image:"../assets/photos/photo14.jpg"
+},
+
+{
+    image:"../assets/photos/photo15.jpg"
+}
+
+];
